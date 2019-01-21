5a21c0cfe1120b66d4e4feab77cab8afe0de51f7
Maciej Stanek <makaron28@gmail.com>
2019-01-21 20:38:58 +0100

    Added some nice plots.

--- plot.py
+++ plot.py
@@ -7,14 +7,22 @@ from scipy.signal import lfilter, butter, filtfilt, medfilt
 headers = ["#", "#moves", "#illegal", "max", "rewards"]
 dat = pd.read_csv('test.csv', names=headers)
 dif = [0] + [j-i for i, j in zip(dat["rewards"][:-1], dat["rewards"][1:])]
+fig = plt.figure()
 
+ax = fig.add_subplot(1, 2, 1)
 a, b = np.polyfit(dat.index, dat['rewards'], 1)
 dif_regr = [a*x+b for x in dat.index]
-print(a)
-print(b)
-#filtr_a, filtr_b = butter(16, 0.2)
-data_to_plot = medfilt(dat['rewards'], 49)
-plt.plot(data_to_plot)
-plt.plot(dat.index, dif_regr)
+med_dif_regr = medfilt(dat['rewards'], 49)
+hard_med_dif_regr = medfilt(dat['rewards'], 599)
+ax.plot(dat.index, dat['rewards'], color='#b4cfef')
+ax.plot(dat.index, med_dif_regr, color='#5b8bc3')
+ax.plot(dat.index, hard_med_dif_regr, color='#235899')
+ax.plot(dat.index, dif_regr, color='red')
+
+ax = fig.add_subplot(1, 2, 2)
+ax.set_yticks([x for x in range(2, 11)])
+ax.set_yticklabels([2**x for x in range(2, 11)])
+logmax = np.log2(dat["max"])
+ax.plot(dat.index, logmax, color='red', marker='o', markersize=0.1, linestyle='')
 
 plt.show()
